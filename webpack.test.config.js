1ac194153278b9bd01ca6f1259d74212e213e8e9
Dan <d.white@zengenti.com>
2017-06-28 11:54:28 +0100

    Added test coverage reporting

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -3,38 +3,33 @@ var path = require('path');
 var rootDir = path.resolve(__dirname);
 
 
-module.exports = {	
+module.exports = {
 
-    devtool: 'source-map',
-
-	entry: {
-        'zengenti.contensis-client': './src/index.ts',
-		'zengenti.contensis-client.spec': './src/index.spec.ts'
-    },
-
-    output: {
-        path: __dirname + "/bundle-test",
-        filename: "[name].js",
-        sourceMapFilename: "[file].map",
-        publicPath: "/bundle-test/",
-
-        library: ['Zengenti', 'Contensis']
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-        loaders: [
-
-        ]
-    },
+	devtool: 'eval-source-map', 
 
 	resolve: {
-        extensions: ['.ts']
-    }
+		extensions: ['.ts', '.js']
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.tsx?$/,
+				enforce: 'post',
+				use: { loader: 'istanbul-instrumenter-loader', options: { esModules: true } },
+				include: path.resolve(__dirname, './src'),
+				exclude: [
+					/\.spec\.ts$/,
+					/\.e2e\.ts$/,
+					/node_modules/
+				]
+			}
+		]
+	}
+
 };
