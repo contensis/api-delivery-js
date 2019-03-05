e1b392545ff2f9af4b9061dbfd58dff6e4b9d8bf
Alex Pop <a.pop@zengenti.com>
2019-03-05 09:20:26 +0000

    Revert "NEW: CONTENSIS-8589 - upgrade all packages except webpack and its dependencies to the latest versions, minimum node version is 8"
    This reverts commit ae636bc630fe9d4e35e0799fa37a92fb2b6394fc.

--- lib/entries/link-resolver.d.ts
+++ lib/entries/link-resolver.d.ts
@@ -6,5 +6,5 @@ export declare class LinkResolver<T extends Entry | Entry[] | PagedList<Entry>> 
     private search;
     constructor(entryOrList: T, paths: string[], versionStatus: VersionStatus, search: (query: any) => Promise<PagedList<Entry>>);
     resolve(): Promise<T>;
-    private getEntries;
+    private getEntries();
 }
